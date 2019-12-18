856d5b1cefa659e73c71107ff3afd1420f828fd3
Rudy N Enriquez <MLEngineer@yahoo.com>
2019-12-18 09:26:17 -0600

    Add class architecture

--- SolverAI/ai.py
+++ SolverAI/ai.py
@@ -1,1 +1,55 @@
+class Sudoku:
 
+    """
+    Sudoku class
+    ------------
+    Solves Sudoku puzzles using various techniques. Can display starting
+    position and solved attempts as a 2-D string
+
+    Parameters
+    -----------
+    puzzle(string): A sudoku puzzle in the format '8...74...1.........7.5.9.4.'
+                    where the . (dot) represents an empty square
+                    puzzle string must have length of 81
+    technique(string): 'single_position' or 'single_candidate'
+                        default = 'single_position'
+                        all techniques use 'single_position'
+    """
+
+    def __init__(self):
+        pass
+
+    def single_position(self):
+        pass
+
+    def single_candidate(self):
+        pass
+
+    def solve(self):
+
+        """
+        Returns a tuple with three elements:
+        1. string - "Solved" or "Not Solved"
+        2. dictionary - Key = The squares coordinate (example: A1)
+                        Value = Number that goes into the sqaure (example: 1)
+        3. string - "Number of iterations made: 5" This is how many times the
+                     algorithm had to iterate through the puzzle
+        """
+        pass
+
+    def show_puzzle(self):
+
+        """
+        Shows the puzzle as a 2-D string with a . (dot) as a placeholder for
+        empty squares.
+        """
+        pass
+
+    def show_solved_attempt(self):
+        
+        """
+        Shows the puzzle as a 2-D string, after attempting a Sudoku technique.
+        If the puzzle cannot be solved, unsolved squares will be replaced by the
+        possible numbers that can fill that square.
+        """
+        pass
